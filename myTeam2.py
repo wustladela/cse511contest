c8f858126eb76a0c0098163de4898beada0eb05e
Adela Gao <adelagao@adelas-mbp.dhcp.wustl.edu>
2015-12-10 12:44:33 -0600

    add index

--- myTeam2.py
+++ myTeam2.py
@@ -43,6 +43,8 @@ class ReflexCaptureAgent(CaptureAgent):
   """
   A base class for reflex agents that chooses score-maximizing actions
   """
+  def __init__ (self, depth='2'):
+    self.depth = int(depth)
 
 
   def chooseAction(self, gameState):
@@ -66,6 +68,12 @@ class ReflexCaptureAgent(CaptureAgent):
     ans = max(result, key = lambda x: x[0])
     return ans[1]
 
+class OffensiveReflexAgent(ReflexCaptureAgent):
+  """
+  A reflex agent that seeks food. This is an agent
+  we give you to get an idea of what an offensive agent might look like,
+  but it is by no means the best or only way to build an offensive agent.
+  """
   def getSuccessor(self, gameState, action):
     """
     Finds the next successor which is a grid position (location tuple).
@@ -193,146 +201,47 @@ class ReflexCaptureAgent(CaptureAgent):
     if newPos in allFood:
       ans = ans + 10000
     return ans
+  
+  # def getFeatures(self, gameState, action):
+  #   features = util.Counter()
+  #   successor = self.getSuccessor(gameState, action)
+  #   features['successorScore'] = self.getScore(successor)
+  #   #agentState1 = gameState.getAgentState(1)
+  #   #print agentState1: #Ghost: (x,y)=(28.0, 4.0), North
+  #   defendingFood = self.getFoodYouAreDefending(successor)#returns a map of all your own food
+  #   getScore = self.getScore(successor)
+  #   print "getScore:"
+  #   print getScore
+  #   prevObs = self.getPreviousObservation()
+  #   redIndex = gameState.getRedTeamIndices()
+  #   print "redIndex:"
+  #   print redIndex
 
-class OffensiveReflexAgent(ReflexCaptureAgent):
-  """
-  A reflex agent that seeks food. This is an agent
-  we give you to get an idea of what an offensive agent might look like,
-  but it is by no means the best or only way to build an offensive agent.
-  """
-  # def nextAgent(self, gameState, agentIndex, depth):
-  #   numAgents = gameState.getNumAgents()
-  #   #wraps around when the whole turn is over
-  #   if agentIndex + 1 == numAgents:
-  #     agentIndex = 0
-  #     depth = self.nextDepth(depth)
-  #   else:
-  #     agentIndex = agentIndex + 1 #increment to the next person's turn
-  #   return agentIndex, depth
+  #   indices = successor.getRedTeamIndices()
+  #   features['getRedTeamIndices'] = indices
+  #   features['getAgentDistances'] = gameState.getAgentDistances()
 
-  # def nextDepth(self, depth):
-  #   if depth < self.depth:
-  #     depth = depth+1
-  #   return depth
-  # def maxValue(self, gameState, agentIndex, depth):
-  #   v = -93372036854775807
-  #   legalActions = gameState.getLegalActions(agentIndex)
-  #   for action in legalActions:
-  #     succState = gameState.generateSuccessor(agentIndex, action)
-  #     v = max(v, self.value(succState, agentIndex, depth))
-  #   return v
+  #   testAgentState = gameState.getAgentState(1) #Ghost: (x,y)=(30.0, 9.0), South...etc
 
-  # def minValue(self, gameState, agentIndex, depth):
-  #   v = 93372036854775807
-  #   legalActions = gameState.getLegalActions(agentIndex)
-  #   for action in legalActions:
-  #     succState = gameState.generateSuccessor(agentIndex, action)
-  #     v = min(v, self.value(succState, agentIndex, depth))
-  #   return v
-
-  # def value(self, gameState, agentIndex, depth):
-  #   if depth == self.depth:
-  #     return self.evaluationFunction(gameState)
-  #   legalActions = gameState.getLegalActions(agentIndex)
-  #   agentIndex, depth = self.nextAgent(gameState, agentIndex, depth)
-  #   ourAgentsIndex = self.getTeam(gameState)
-  #   if agentIndex in ourAgentsIndex: #assuming we move first
-  #     return self.maxValue(gameState, agentIndex, depth)
-  #   else:
-  #     return self.minValue(gameState, agentIndex, depth)
-
-  # def evaluationFunction(self, currentGameState, agentIndex, action):
-  #   from searchAgents import mazeDistance
-  #   """
-  #   The evaluation function takes in the current and proposed successor
-  #   GameStates (pacman.py) and returns a number, where higher numbers are better.
-
-  #   Remaining food (newFood) and Pacman position after moving (newPos).
-  #   """
-  #   successorGameState = currentGameState.generateSuccessor(agentIndex, action)
-  #   newPos = successorGameState.getAgentPosition(agentIndex)
-  #   newFood = self.getFood(successorGameState)
-  #   newGhostStates = successorGameState.getGhostStates()
-  #   newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-  #   "make sure it keeps eating all the food... and do not stop if the food is close"
-  #   "get ALL the food"
-  #   "get to the closest food and EAT it"
-  #   """
-  #   TODO:
-  #   Now we have 2 agents! 
-  #   food is the other section of food
-  #   adjust the index to do this
-  #   the minimax agent is complete, 
-  #   """
-  #   allFood = self.getFood(currentGameState).asList()
-  #   #allFood = currentGameState.getFood().asList()
-  #   foodDistance = []
-  #   ans = self.getScore()
-  #   for dot in allFood:
-  #       distance = self.getMazeDistance(newPos, dot)
-  #       item = (dot, distance)
-  #       foodDistance.append(item)
-  #   #check ALL ghosts
-  #   numAgents = successorGameState.getNumAgents()
-  #   allGhostPositions = []
-
-  #   opponentIndex = self.getOpponents(successorGameState)
-  #   for eachOppo in opponentIndex:
-  #     ghostNewPos = successorGameState.getAgentPosition(eachOppo)
-  #     if ghostNewPos is not None:
-  #       allGhostPositions.append(ghostNewPos)
-  #   for eachGhost in allGhostPositions:
-  #     ghostDistance = self.getMazeDistance(newPos, eachGhost)
-  #     if ghostDistance<4:
-  #       ans = ans - (3-ghostDistance)*100000
-
-  #   if len(foodDistance)>0:
-  #     closestFood = min(foodDistance, key = lambda x: x[1])
-  #     ans = ans - closestFood[1]
-  #   if newPos in allFood:
-  #     ans = ans + 10000
-  #   return ans
-
-  def getFeatures(self, gameState, action):
-    features = util.Counter()
-    successor = self.getSuccessor(gameState, action)
-    features['successorScore'] = self.getScore(successor)
-    #agentState1 = gameState.getAgentState(1)
-    #print agentState1: #Ghost: (x,y)=(28.0, 4.0), North
-    defendingFood = self.getFoodYouAreDefending(successor)#returns a map of all your own food
-    getScore = self.getScore(successor)
-    print "getScore:"
-    print getScore
-    prevObs = self.getPreviousObservation()
-    redIndex = gameState.getRedTeamIndices()
-    print "redIndex:"
-    print redIndex
-
-    indices = successor.getRedTeamIndices()
-    features['getRedTeamIndices'] = indices
-    features['getAgentDistances'] = gameState.getAgentDistances()
-
-    testAgentState = gameState.getAgentState(1) #Ghost: (x,y)=(30.0, 9.0), South...etc
-
-    for i in gameState.getRedTeamIndices():
-        if gameState.isOnRedTeam(i):
-            pos = gameState.getAgentPosition(i)
-            features["agent ",i," state"] = gameState.getAgentState(i)
-    print "features"
-    print features
+  #   for i in gameState.getRedTeamIndices():
+  #       if gameState.isOnRedTeam(i):
+  #           pos = gameState.getAgentPosition(i)
+  #           features["agent ",i," state"] = gameState.getAgentState(i)
+  #   print "features"
+  #   print features
 
 
-    # Compute distance to the nearest food
-    foodList = self.getFood(successor).asList()
-    if len(foodList) > 0: # This should always be True,  but better safe than sorry
-      myPos = successor.getAgentState(self.index).getPosition()
-      minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
-      features['distanceToFood'] = minDistance
+  #   # Compute distance to the nearest food
+  #   foodList = self.getFood(successor).asList()
+  #   if len(foodList) > 0: # This should always be True,  but better safe than sorry
+  #     myPos = successor.getAgentState(self.index).getPosition()
+  #     minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+  #     features['distanceToFood'] = minDistance
 
-    return features
+  #   return features
 
-  def getWeights(self, gameState, action):
-    return {'successorScore': 100, 'distanceToFood': -1}
+  # def getWeights(self, gameState, action):
+  #   return {'successorScore': 100, 'distanceToFood': -1}
 
 class DefensiveReflexAgent(ReflexCaptureAgent):
   """
